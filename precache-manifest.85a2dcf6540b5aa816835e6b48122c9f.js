b6dc2f329a33f46b872b537d9f6b8523c50a7f8e
ci-build <ci-build@myclimateaction.com>
2019-10-14 14:57:01 +0000

    Updates

--- precache-manifest.85a2dcf6540b5aa816835e6b48122c9f.js
+++ precache-manifest.85a2dcf6540b5aa816835e6b48122c9f.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4e0ee3fc7bb97c18368ee6616727923a",
+    "revision": "60002b5cbac1966b743b99ef68842dda",
     "url": "/myclimateaction-admin/index.html"
   },
   {
-    "revision": "b5a5a04659bc41d15ffb",
-    "url": "/myclimateaction-admin/static/css/main.1b2d4935.chunk.css"
+    "revision": "444fd9a8fd790ee373d4",
+    "url": "/myclimateaction-admin/static/css/main.cdfef2f1.chunk.css"
   },
   {
     "revision": "e95f744a915cbe024853",
     "url": "/myclimateaction-admin/static/js/2.2d1d90bf.chunk.js"
   },
   {
-    "revision": "b5a5a04659bc41d15ffb",
+    "revision": "444fd9a8fd790ee373d4",
     "url": "/myclimateaction-admin/static/js/main.b8ae2786.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myclimateaction-admin/precache-manifest.5dba68d4e7dc21b3716563b6b499d432.js"
+  "/myclimateaction-admin/precache-manifest.85a2dcf6540b5aa816835e6b48122c9f.js"
 );
 
 self.addEventListener('message', (event) => {
